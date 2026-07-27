2ae56d04129d2c2718a94f02d5c66b85954e0a3b
YusifAliyef <yusife507@gmail.com>
2026-07-27 16:47:11 +0400

    week 2 - feat: Checkpoint 5 - Auth xəta cavablarının (401 vs 403) standartlaşdırılması

--- src/middlewares/roleMiddleware.ts
+++ src/middlewares/roleMiddleware.ts
@@ -3,16 +3,19 @@ import { UserRole } from "../entities/User";
 
 export const authorizeRoles = (...allowedRoles: UserRole[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
+    // 1. Token yoxdursa -> 401 Unauthorized
     if (!req.user) {
-      return res
-        .status(401)
-        .json({ message: "Giriş icazəsi yoxdur: Istifadəçi tapılmadı!" });
+      return res.status(401).json({ 
+        error: "Unauthorized", 
+        message: "Giriş icazəsi yoxdur: İstifadəçi tapılmadı!" 
+      });
     }
 
+    // 2. Token var, amma rolu yetərli deyilsə -> 403 Forbidden
     if (!allowedRoles.includes(req.user.role)) {
-      return res.status(403).json({
-        message:
-          "Giriş qadağandır: Bu əməliyyatı icra etmək üçün yetərli icazəniz (rolunuz) yoxdur!",
+      return res.status(403).json({ 
+        error: "Forbidden", 
+        message: "Giriş qadağandır: Bu əməliyyat üçün kifayət qədər səlahiyyətiniz yoxdur!" 
       });
     }
 
